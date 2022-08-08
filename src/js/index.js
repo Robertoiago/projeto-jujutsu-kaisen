c45a2af37ad14a64335130c5d58b5033574c0dd9
Robertoiago <108899672+Robertoiago@users.noreply.github.com>
2022-08-07 21:00:37 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,50 @@
+/*
+
+objetivo 1 - quando clicarmos na seta de avançar temos que mostra o proximo cartão da lista
+- passoa 1 - dar um jeito de pegar o elemento HTML da seta avançar
+- passo 2 dar jeito de indentificar o clique do usuário na seta avançar
+- passo 3 fazer aparecer o proximo cartao da lista 
+- passo 4 - buscar o cartao que esta selecionado e esconder
+
+objetivo 2 - quando clicarmos na seta de voltar temos que mostra o cartão anterior da lista
+- passoa 1 - dar um jeito de pegar o elemento HTML da seta voltar
+- passo 2 dar jeito de indentificar o clique do usuário na seta voltar
+- passo 3 fazer aparecer o cartao anterior da lista 
+- passo 4 - buscar o cartao que esta selecionado e esconder*/
+
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.
+getElementById('btn-voltar');
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado(){
+const cartaoSelecionado = document.querySelector(".selecionado");
+cartaoSelecionado.classList.remove("selecionado");
+}
+
+function mostrarCartao(indiceCartao){
+    cartoes[indiceCartao].classList.add("selecionado");
+}
+
+btnAvancar.addEventListener("click", function () { 
+    if(cartaoAtual === cartoes.length - 1) return;
+
+esconderCartaoSelecionado();
+
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);
+
+  
+});
+
+btnVoltar.addEventListener("click", function () {
+
+    if(cartaoAtual === 0) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
+
+});
